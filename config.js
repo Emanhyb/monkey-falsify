9b21f4c20e935967a98b58097a50fafe373edf7c
Emanhyb <153253184+Emanhyb@users.noreply.github.com>
2025-02-20 13:47:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.config = {
-  entryPhrase: "getmein",  // phrase to enter the real site
-  proxyLink: "https://example.com", // proxy link, highly recommended you change it
+  entryPhrase: "EMAN",  // phrase to enter the real site
+  proxyLink: "https://gyat-five.vercel.app", // proxy link, highly recommended you change it
   blanker: "byebye", // opens your proxy link in about blank
   leverofdoom: "no",
-  alternativeproxy: "https://youtube.com", // alternative Proxy
-  useproxy: "false" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies (not in current use)
+  alternativeproxy: "interstellar-ten-green.vercel.app", // alternative Proxy
+  useproxy: "tru" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies (not in current use)
 };
